32223c222833908d4a1a63d2782f6b94abb1d394
shafnap123 <shafnasaleem980@gmail.com>
2025-09-09 02:52:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+// let a=true;
+// if (a==true){
+//     console.log("hllo")
+    
+//     }
+//     else{
+//         console.log("hi")
+// }
+//  var b=true;
+
+//  b==true? console.log("this condition is true"):console.log("thi condition is false");
+
+//  var n=b;
+//  n==true? console.log("true"):console.log("false")
+// var v=false;
+// v==true?console.log("true"):console.log("false")
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(40,70)
+
+// (function(){
+//     console.log("hlo welcome")
+// // })()
+// (function(){
+//     console.log("lilu")
+// })()
+// const person={?
+//     name:"shafna",
+//     place:"malappuram",
+//     phonenumber:"78567844"
+// }
+// console.log(person["place"])
+// function myclick(){
+//     document.querySelector(".aa").innerHTML="my name is shana"
+// }
+// const y=12;
+// let an=`hloworld ${y}`
+// console.log(an)
+// const o=90;
+// let find=`myname+${o}`
+// console.log(find)
+// let text="welcome to javascript"
+
+// console.log(text.toUpperCase())
+// function myclick(){
+//     let a="welcome to html"
+//     let an=a.replace("html","javascript")
+//     console.log(an)
+// }
+// let array=[1,2,3]
+// r=array[3]=10;
+// console.log(r)
+// let array=[1,2,3]
+// r=replace(3)
+// console.log(r)
+// let array=[1,"sss",7]
+// array.pop()
+// console.log(array)
+// let array=[1,2,3]
+// array.splice(3,0,"ee","rr")
+// console.log(array)
+// let array1=[1,2,3]
+// let array2=["ww","ww","aa"]
+// an=array1.concat(array2)
+// console.log(an.includes("a"))
+// let array=[1,66,77,8,9,44,7]
+// let newarray=array.map((value,index)=>value*3)
+
+// console.log(newarray)
+// console.log(array)
+// let array=[1,3,6,7,9]
+//  newarray=array.filter((value,index)=>value<3)
+//  console.log(newarray)
+//  let array=[1,5,6,7]
+// newarray= array.find((value,index)=>value>5)
+// console.log(newarray)
+let array=[3,7,8,9,8]
+newarray=array.reduce((sum,value)=>sum+value,0)
+console.log(newarray)
